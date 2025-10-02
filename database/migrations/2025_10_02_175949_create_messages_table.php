eace5070650ced5641cb64cd53b43b1735aea62d
giovernando <giovernando24@gmail.com>
2025-10-03 01:09:41 +0700

    message

--- database/migrations/2025_10_02_175949_create_messages_table.php
+++ database/migrations/2025_10_02_175949_create_messages_table.php
@@ -4,24 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('subject');
+            $table->string('email');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->longText('message');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('messages');
     }
-};
+}
