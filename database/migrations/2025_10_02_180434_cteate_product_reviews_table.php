ffd5d0e2a5ab14caedbcc3c81371e2907705dd00
giovernando <giovernando24@gmail.com>
2025-10-03 01:12:24 +0700

    w

--- database/migrations/2025_10_02_180434_cteate_product_reviews_table.php
+++ database/migrations/2025_10_02_180434_cteate_product_reviews_table.php
@@ -4,21 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductReviewsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::create('product_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->tinyInteger('rate')->default(0);
+            $table->text('review')->nullable();
+            $table->enum('status',['active','inactive'])->default('active');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('SET NULL');
+            $table->timestamps();
+        });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::dropIfExists('product_reviews');
     }
-};
+}
