e3ce03218b7f1752df1357ca5d67db4a1c8da2d3
giovernando <giovernando24@gmail.com>
2025-10-03 01:12:01 +0700

    wish

--- database/migrations/2025_10_02_180407_create_wishlists_table.php
+++ database/migrations/2025_10_02_180407_create_wishlists_table.php
@@ -4,24 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateWishlistsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('cart_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->float('price');
+            $table->integer('quantity');
+            $table->float('amount');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('cart_id')->references('id')->on('carts')->onDelete('SET NULL');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('wishlists');
     }
-};
+}
