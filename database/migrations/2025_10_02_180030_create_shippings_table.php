490c65517125b7150f85e19a98862798c9fe8a36
giovernando <giovernando24@gmail.com>
2025-10-03 01:10:00 +0700

    shipping

--- database/migrations/2025_10_02_180030_create_shippings_table.php
+++ database/migrations/2025_10_02_180030_create_shippings_table.php
@@ -4,24 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateShippingsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('shippings', function (Blueprint $table) {
             $table->id();
+            $table->string('type');
+            $table->decimal('price');
+            $table->enum('status',['active','inactive'])->default('active');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('shippings');
     }
-};
+}
