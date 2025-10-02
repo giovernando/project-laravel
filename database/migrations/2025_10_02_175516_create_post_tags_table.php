9ce50c251ceb255394277f28be92129cf5fc8baf
giovernando <giovernando24@gmail.com>
2025-10-03 01:08:44 +0700

    tags and categories

--- database/migrations/2025_10_02_175516_create_post_tags_table.php
+++ database/migrations/2025_10_02_175516_create_post_tags_table.php
@@ -4,24 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePostTagsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('post_tags', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->enum('status',['active','inactive'])->default('active');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('post_tags');
     }
-};
+}
