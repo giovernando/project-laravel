b5825de839ed8267fec1f6236deb5043478125a3
giovernando <giovernando24@gmail.com>
2025-10-12 22:08:00 +0700

    update selesai

--- config/services.php
+++ config/services.php
@@ -14,12 +14,14 @@ return [
     |
     */
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
-    'resend' => [
-        'key' => env('RESEND_KEY'),
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
     ],
 
     'ses' => [
@@ -27,12 +29,20 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
-    ],
+    'github' => [
+        'client_id' => '24fbd0edd46019058da7', //Github API
+        'client_secret' => '4500e6ab7a9bb70608600fbdee37969276bb0685', //Github Secret
+        'redirect' => 'http://localhost:8000/login/github/callback',
+     ],
+     'google' => [
+        'client_id' => '122886334250-fsfd8ugugkr0hjrkakukbgnnruomjn95.apps.googleusercontent.com', //Google API
+        'client_secret' => 'eyuwbzXGBgbebzPXNiqSOods', //Google Secret
+        'redirect' => 'http://localhost:8000/login/google/callback',
+     ],
+     'facebook' => [
+        'client_id' => '1494792524037846', //Facebook API
+        'client_secret' => '1bf13c2faf680f4ae2b97bdafa92c7d2', //Facebook Secret
+        'redirect' => 'http://localhost:8000/login/facebook/callback',
+     ],
 
 ];
